df08813fc6c87c876b2efe74d2c9487230acc0c4
Jarvis Mainframe <thomasjrideriii@gmail.com>
2020-08-10 15:54:35 -0400

    transfer from older repo

---
+++ build/precache-manifest.686e728c596e9b317ec79f6425d39b3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a0a65425523b4db61f17519b9c31048",
+    "url": "/thomasjrideriii.github.io/index.html"
+  },
+  {
+    "revision": "b895886c4a3283d70481",
+    "url": "/thomasjrideriii.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1bb0266c79448c5ed10f",
+    "url": "/thomasjrideriii.github.io/static/js/2.fd9e7cd5.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/thomasjrideriii.github.io/static/js/2.fd9e7cd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b895886c4a3283d70481",
+    "url": "/thomasjrideriii.github.io/static/js/main.69aa9ea7.chunk.js"
+  },
+  {
+    "revision": "c07f693b96e414194c39",
+    "url": "/thomasjrideriii.github.io/static/js/runtime-main.4109f6cb.js"
+  },
+  {
+    "revision": "cdcfd5a44bc950fa09d8eeeb57eefea6",
+    "url": "/thomasjrideriii.github.io/static/media/profile.cdcfd5a4.jpg"
+  }
+]);
